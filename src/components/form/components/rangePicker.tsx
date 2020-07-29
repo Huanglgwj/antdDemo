838cf424f7b0e8bef498439c69fbb45edda9078d
Why <879771632@qq.com>
2020-07-29 10:56:05 +0800

    修改bug

--- src/components/form/components/rangePicker.tsx
+++ src/components/form/components/rangePicker.tsx
@@ -1,51 +1,57 @@
-import React from "react";
-import { DatePicker } from "antd";
-import moment from "moment";
+import React from 'react';
+import { DatePicker } from 'antd';
+import moment from 'moment';
 const { RangePicker } = DatePicker;
 // 时间区间
 interface rangePickerProps {
-  handleChange: any;
-  columns: objectKey;
-  initialValues?: objectKey;
+    handleChange: any;
+    columns: objectKey;
+    initialValues?: objectKey;
 }
-let styleOption = { width: "270px" };
-const RangePickerModule: React.FC<rangePickerProps> = props => {
-  let { handleChange, columns, initialValues = {} } = props;
-  let [defaultValue, setDefaultValue] = React.useState<any>(null);
+let styleOption = { width: '270px' };
+const RangePickerModule: React.FC<rangePickerProps> = (props) => {
+    let { handleChange, columns, initialValues = {} } = props;
+    let [defaultValue, setDefaultValue] = React.useState<any>(null);
+    let [defaultValueString, setDefaultValueString] = React.useState<string[]>([]);
 
     // 设置默认值
     React.useEffect(() => {
-        if (initialValues[columns.name] && Object.prototype.toString.call(initialValues[columns.name]) === '[object Array]') {
-          let format = "YYYY-MM-DD";
-          if (columns.params && columns.params.format) {
-            format = columns.params.format;
-          }
-    
-          initialValues[columns.name] = initialValues[columns.name].map(
-            (item: objectKey) => {
-              return moment(item, format);
+        if (
+            initialValues &&
+            initialValues[columns.name] &&
+            Object.prototype.toString.call(initialValues[columns.name]) === '[object Array]' &&
+            JSON.stringify(initialValues[columns.name]) !== JSON.stringify(defaultValueString)
+        ) {
+            // 设置格式
+            let format = 'YYYY-MM-DD';
+            // 如果有动态传格式，则替换
+            if (columns.params && columns.params.format) {
+                format = columns.params.format;
             }
-          );
-          setDefaultValue(initialValues[columns.name]);
+            initialValues[columns.name] = initialValues[columns.name].map((item: objectKey) => {
+                return moment(item, format);
+            });
+            setDefaultValue(initialValues[columns.name]);
         }
-      }, [initialValues]);
-  const valueChange = (value: any, dateString: any) => {
-    // 没有值时返回空数组
-    if (!dateString[0] || !dateString[1]) {
-      dateString = [];
-    }
-    handleChange(dateString, columns.name);
-    setDefaultValue(value);
-  };
-  return (
-    <RangePicker
-      value={defaultValue}
-      className={`${columns.className || ""} form-time-pickert`}
-      format={"YYYY-MM-DD"}
-      style={{ ...styleOption, ...(columns.style || {}) }}
-      onChange={valueChange}
-      {...(columns.params || {})}
-    />
-  );
+    }, [initialValues]);
+    const valueChange = (value: any, dateString: any) => {
+        // 没有值时返回空数组
+        if (!dateString[0] || !dateString[1]) {
+            dateString = [];
+        }
+        handleChange(dateString, columns.name);
+        setDefaultValue(value);
+        setDefaultValueString(dateString);
+    };
+    return (
+        <RangePicker
+            value={defaultValue}
+            className={`${columns.className || ''} form-time-pickert`}
+            format={'YYYY-MM-DD'}
+            style={{ ...styleOption, ...(columns.style || {}) }}
+            onChange={valueChange}
+            {...(columns.params || {})}
+        />
+    );
 };
 export default RangePickerModule;
--- src/components/search/components/rangePicker.tsx
+++ src/components/search/components/rangePicker.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { DatePicker } from 'antd';
 import moment from 'moment';
@@ -12,23 +11,31 @@ interface rangePickerProps {
 let styleOption = { width: '270px' };
 const RangePickerModule: React.FC<rangePickerProps> = (props) => {
     let { handleChange, columns, initialValues = {} } = props;
-    let [defaultValue, setDefaultValue] = React.useState<any>(null);
+    // 当前值-时间格式
+    let [defaultValue, setDefaultValue] = React.useState<any>([]);
+    // 当前值-字符串格式
+    let [defaultValueString, setDefaultValueString] = React.useState<any>([]);
 
     // 设置默认值
     React.useEffect(() => {
-        if (initialValues && initialValues[columns.name] !== defaultValue) {
-            initialValues[columns.name] = initialValues[columns.name] || []
+        if (
+            initialValues &&
+            initialValues[columns.name] &&
+            Object.prototype.toString.call(initialValues[columns.name]) === '[object Array]' &&
+            JSON.stringify(initialValues[columns.name]) !== JSON.stringify(defaultValueString)
+        ) {
+            let _initialValues = JSON.parse(JSON.stringify(initialValues));
+            _initialValues[columns.name] = _initialValues[columns.name] || [];
             // 设置格式
             let format = 'YYYY-MM-DD';
             // 如果有动态传格式，则替换
             if (columns.params && columns.params.format) {
                 format = columns.params.format;
             }
-            initialValues[columns.name] = initialValues[columns.name].map((item: objectKey) => {
+            _initialValues[columns.name] = _initialValues[columns.name].map((item: objectKey) => {
                 return moment(item, format);
             });
-           
-            setDefaultValue(initialValues[columns.name]);
+            setDefaultValue(_initialValues[columns.name]);
         }
     }, [initialValues]);
     const valueChange = (value: any, dateString: any) => {
@@ -38,18 +45,19 @@ const RangePickerModule: React.FC<rangePickerProps> = (props) => {
         }
         handleChange(dateString, columns.name);
         setDefaultValue(value);
+        setDefaultValueString(dateString);
     };
     return (
         <div className={`${columns.className || ''} dis-inb`}>
-        <span className="col-9">{columns.title}</span>
-        <RangePicker
-            value={defaultValue}
-            className={`${columns.className || ''} form-time-pickert ml-5`}
-            format={'YYYY-MM-DD'}
-            style={{ ...styleOption, ...(columns.style || {}) }}
-            onChange={valueChange}
-            {...(columns.params || {})}
-        />
+            <span className="col-9">{columns.title}</span>
+            <RangePicker
+                value={defaultValue}
+                className={`${columns.className || ''} form-time-pickert ml-5`}
+                format={'YYYY-MM-DD'}
+                style={{ ...styleOption, ...(columns.style || {}) }}
+                onChange={valueChange}
+                {...(columns.params || {})}
+            />
         </div>
     );
 };
