86bb964434288f41f3053bfd3332237ce85c2c3c
Why <879771632@qq.com>
2020-08-05 17:58:34 +0800

    修改config

--- config-overrides.js
+++ config-overrides.js
@@ -4,40 +4,94 @@ const {
   addWebpackAlias,
   addWebpackPlugin,
   addWebpackExternals,
-  addLessLoader
-} = require("customize-cra");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
-const path = require("path");
-process.env.GENERATE_SOURCEMAP = "false";
-module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true //或者css, true代表运用less
-  }),
-  addLessLoader({
-    lessOptions:{javascriptEnabled:true}
-  }),
-  // 别名
-  addWebpackAlias({
-    "@": path.resolve(__dirname, "src")
-  }),
-  // 显示打包后的文件体积
-  // addWebpackPlugin(
-  //     new BundleAnalyzerPlugin({
-  //         analyzerMode: 'static', //输出静态报告文件report.html，而不是启动一个web服务
-  //     }),
-  // ),
-  // 替换moment.js
-  addWebpackPlugin(new AntdDayjsWebpackPlugin()),
-  // cnd引入的不打包
-  // addWebpackExternals({
-  //     react: 'React',
-  //     'react-dom': 'ReactDom',
-  // }),
-  //暴露webpack的配置
-  config => {
-    return config;
-  }
-);
+  addLessLoader,
+  overrideDevServer,
+} = require('customize-cra');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const path = require('path');
+// 是否生产环境
+const production = process.env.NODE_ENV === 'production';
+// 生产环境取消sourcemap
+production ? (process.env.GENERATE_SOURCEMAP = 'false') : '';
+// devServer设置
+const addProxy = () => (configFunction) => {
+  configFunction.host = '127.0.0.1';
+  configFunction.port = 3000;
+  configFunction.hotOnly = false;
+  configFunction.proxy = {
+      '/taskapi': {
+          target: 'http://47.101.212.213',
+          changeOrigin: true,
+          pathRewrite: { '^/taskapi': '' },
+      },
+  };
+  return configFunction;
+};
+module.exports = {
+  webpack: override(
+      // 模块化导入
+      fixBabelImports('import', {
+          libraryName: 'antd',
+          libraryDirectory: 'es',
+      }),
+      // less配置
+      addLessLoader({
+          lessOptions: { javascriptEnabled: true },
+      }),
+      // 别名
+      addWebpackAlias({
+          '@': path.resolve(__dirname, 'src'),
+      }),
+      // 生产环境显示打包后的文件体积
+      production
+          ? addWebpackPlugin(
+                new BundleAnalyzerPlugin({
+                    analyzerMode: 'static', //输出静态报告文件report.html，而不是启动一个web服务
+                }),
+            )
+          : '',
+      // 替换moment.js
+      addWebpackPlugin(new AntdDayjsWebpackPlugin()),
+      // cnd引入的不打包
+      // addWebpackExternals({
+      //     react: 'React',
+      //     'react-dom': 'ReactDom',
+      // }),
+      //暴露webpack的配置
+      (config) => {
+          // 抽离公共代码
+          config.optimization.splitChunks = {
+              chunks: 'all',
+              name: 'vender',
+              cacheGroups: {
+                  vender: {
+                      name: 'vendor',
+                      test: /[\\/]node_modules[\\/]/,
+                      chunks: 'all',
+                      priority: 10,
+                      enforce: true,
+                  },
+                  react: {
+                      name: 'react',
+                      test: (module) => /react|redux/.test(module.context),
+                      chunks: 'initial',
+                      priority: 11,
+                      enforce: true,
+                  },
+                  antd: {
+                      name: 'antd',
+                      test: (module) => {
+                          return /ant/.test(module.context);
+                      },
+                      chunks: 'initial',
+                      priority: 11,
+                      enforce: true,
+                  },
+              },
+          };
+          return config;
+      },
+  ),
+  devServer: overrideDevServer(addProxy()),
+};
